08d2ad56df93cef1e48c6ed9147d0cef73fc2f55
Shubham <shubhwankar88@gmail.com>
2025-08-18 20:27:43 +0530

    spring security added

--- src/main/java/com/demo/journalApp/Config/SecurityConfig.java
+++ src/main/java/com/demo/journalApp/Config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.demo.journalApp.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,12 +15,16 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    public UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,23 +47,33 @@ public class SecurityConfig {
     // but now we are providing our own implementation of UserDetailsService
     // here we are telling that how it will verify our username and password
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//
+//        UserDetails user1 = User
+//                .withDefaultPasswordEncoder()
+//                .username("kiran")
+//                .password("k123")
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails user2 = User
+//                .withDefaultPasswordEncoder()
+//                .username("vaibhav")
+//                .password("v123")
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+    // now we want our username and password come from database
     @Bean
-    public UserDetailsService userDetailsService() {
-
-        UserDetails user1 = User
-                .withDefaultPasswordEncoder()
-                .username("kiran")
-                .password("k123")
-                .roles("USER")
-                .build();
-
-        UserDetails user2 = User
-                .withDefaultPasswordEncoder()
-                .username("vaibhav")
-                .password("v123")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
     }
+
 }
---
+++ src/main/java/com/demo/journalApp/Service/MyUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.demo.journalApp.Service;
+
+import com.demo.journalApp.Entity.User;
+import com.demo.journalApp.Entity.UserPrincipal;
+import com.demo.journalApp.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        if(user == null){
+            System.out.println("user not found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
+    }
+}
